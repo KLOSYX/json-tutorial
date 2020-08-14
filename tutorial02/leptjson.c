f3cb0a652aa4111d5065ce3bfbe9f603fbe0042a
anbinx <klosyx@outlook.com>
2020-08-14 14:47:39 +0800

    Refactor parase function

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -15,30 +15,27 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
-static int lept_parse_true(lept_context* c, lept_value* v) {
-    EXPECT(c, 't');
-    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_TRUE;
-    return LEPT_PARSE_OK;
-}
-
-static int lept_parse_false(lept_context* c, lept_value* v) {
-    EXPECT(c, 'f');
-    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 4;
-    v->type = LEPT_FALSE;
-    return LEPT_PARSE_OK;
-}
-
-static int lept_parse_null(lept_context* c, lept_value* v) {
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-        return LEPT_PARSE_INVALID_VALUE;
-    c->json += 3;
-    v->type = LEPT_NULL;
+static int lept_parse_literal(lept_context* c, lept_value* v, const char* ch){
+    int i = 0;
+    assert(ch != NULL);
+    EXPECT(c, ch[0]);
+    while (ch[i + 1] != '\0'){
+        if (c->json[i] != ch[i + 1])  return LEPT_PARSE_INVALID_VALUE;
+        ++i;
+    }
+    c->json += i;
+    switch (ch[0])
+    {
+    case 't':
+        v->type = LEPT_TRUE;
+        break;
+    case 'f':
+        v->type = LEPT_FALSE;
+        break;
+    default:
+        v->type = LEPT_NULL;
+        break;
+    }
     return LEPT_PARSE_OK;
 }
 
@@ -55,9 +52,9 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
 
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
-        case 't':  return lept_parse_true(c, v);
-        case 'f':  return lept_parse_false(c, v);
-        case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_literal(c, v, "true");
+        case 'f':  return lept_parse_literal(c, v, "false");
+        case 'n':  return lept_parse_literal(c, v, "null");
         default:   return lept_parse_number(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
     }
