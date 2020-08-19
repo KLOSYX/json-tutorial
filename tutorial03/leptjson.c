7f4f4151d68e5d1b3de8c7c60323f0c86e70c1a3
anbinx <klosyx@outlook.com>
2020-08-19 23:20:47 +0800

    Fix memory leak

--- tutorial03/leptjson.c
+++ tutorial03/leptjson.c
@@ -104,37 +104,24 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
                 return LEPT_PARSE_MISS_QUOTATION_MARK;
             case '\\':
                 switch (*p++) {
-                case 'n':
-                    PUTC(c, '\n');
-                    break;
-                case 'b':
-                    PUTC(c, '\b');
-                    break;
-                case 'f':
-                    PUTC(c, '\f');
-                    break;
-                case 'r':
-                    PUTC(c, '\r');
-                    break;
-                case 't':
-                    PUTC(c, '\t');
-                    break;
-                case '\\':
-                    PUTC(c, '\\');
-                    break;
-                case '\"':
-                    PUTC(c, '\"');
-                    break;
-                case '/':
-                    PUTC(c, '/');
-                    break;
-                default:
-                    return LEPT_PARSE_INVALID_STRING_ESCAPE;
-                    break;
+                    case 'n':   PUTC(c, '\n'); break;
+                    case 'b':   PUTC(c, '\b'); break;
+                    case 'f':   PUTC(c, '\f'); break;
+                    case 'r':   PUTC(c, '\r'); break;
+                    case 't':   PUTC(c, '\t'); break;
+                    case '\\':  PUTC(c, '\\'); break;
+                    case '\"':  PUTC(c, '\"'); break;
+                    case '/':   PUTC(c, '/'); break;
+                    default: 
+                        c->top = head;
+                        return LEPT_PARSE_INVALID_STRING_ESCAPE;
                 }
                 break;
             default:
-                if (ch >= 0 && ch <= 31) return LEPT_PARSE_INVALID_STRING_CHAR;
+                if (ch >= 0 && ch <= 31) {
+                    c->top = head;
+                    return LEPT_PARSE_INVALID_STRING_CHAR;
+                }
                 PUTC(c, ch);
         }
     }
@@ -191,7 +178,7 @@ int lept_get_boolean(const lept_value* v) {
 }
 
 void lept_set_boolean(lept_value* v, int b) {
-    assert(v != NULL && (b == 0 || b == 1));
+    lept_free(v);
     if (b)  v->type = LEPT_TRUE;
     else v->type = LEPT_FALSE;
 }
@@ -202,7 +189,7 @@ double lept_get_number(const lept_value* v) {
 }
 
 void lept_set_number(lept_value* v, double n) {
-    assert( v!= NULL);
+    lept_free(v);
     v->type = LEPT_NUMBER;
     v->u.n = n;
 }
